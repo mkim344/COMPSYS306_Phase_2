9aab2466a33231a8d0c95ea40ded7973b9a45ce2
jjun273 <jjun273@aucklanduni.ac.nz>
2022-10-20 16:47:02 +1300

    Change image size and add new road category

--- SVM.py
+++ SVM.py
@@ -46,8 +46,8 @@ def resize_all(src, pklname, include, width=150, height=None):
 print(os.listdir(src))
 
 base_name = 'traffic_signs'
-width = 80
-include = {'green_light', 'red_light', 'stop_sign', 'thirty_sign'}
+width = 150
+include = {'green_light', 'red_light', 'stop_sign', 'thirty_sign', 'road'}
 resize_all(src=src, pklname=base_name, width=width, include=include)
 
 
@@ -74,54 +74,54 @@ X_train, X_test, y_train, y_test = train_test_split(
     random_state=42,
 )
 
-class RGB2GrayTransformer(BaseEstimator, TransformerMixin):
-    """
-    Convert an array of RGB images to grayscale
-    """
-
-    def __init__(self):
-        pass
-
-    def fit(self, X, y=None):
-        """returns itself"""
-        return self
-
-    def transform(self, X, y=None):
-        """perform the transformation and return an array"""
-        return np.array([skimage.color.rgb2gray(img) for img in X])
-
-
-class HogTransformer(BaseEstimator, TransformerMixin):
-    """
-    Expects an array of 2d arrays (1 channel images)
-    Calculates hog features for each img
-    """
-
-    def __init__(self, y=None, orientations=9,
-                 pixels_per_cell=(8, 8),
-                 cells_per_block=(3, 3), block_norm='L2-Hys'):
-        self.y = y
-        self.orientations = orientations
-        self.pixels_per_cell = pixels_per_cell
-        self.cells_per_block = cells_per_block
-        self.block_norm = block_norm
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X, y=None):
-
-        def local_hog(X):
-            return hog(X,
-                       orientations=self.orientations,
-                       pixels_per_cell=self.pixels_per_cell,
-                       cells_per_block=self.cells_per_block,
-                       block_norm=self.block_norm)
-
-        try:  # parallel
-            return np.array([local_hog(img) for img in X])
-        except:
-            return np.array([local_hog(img) for img in X])
+# class RGB2GrayTransformer(BaseEstimator, TransformerMixin):
+#     """
+#     Convert an array of RGB images to grayscale
+#     """
+#
+#     def __init__(self):
+#         pass
+#
+#     def fit(self, X, y=None):
+#         """returns itself"""
+#         return self
+#
+#     def transform(self, X, y=None):
+#         """perform the transformation and return an array"""
+#         return np.array([skimage.color.rgb2gray(img) for img in X])
+#
+#
+# class HogTransformer(BaseEstimator, TransformerMixin):
+#     """
+#     Expects an array of 2d arrays (1 channel images)
+#     Calculates hog features for each img
+#     """
+#
+#     def __init__(self, y=None, orientations=9,
+#                  pixels_per_cell=(8, 8),
+#                  cells_per_block=(3, 3), block_norm='L2-Hys'):
+#         self.y = y
+#         self.orientations = orientations
+#         self.pixels_per_cell = pixels_per_cell
+#         self.cells_per_block = cells_per_block
+#         self.block_norm = block_norm
+#
+#     def fit(self, X, y=None):
+#         return self
+#
+#     def transform(self, X, y=None):
+#
+#         def local_hog(X):
+#             return hog(X,
+#                        orientations=self.orientations,
+#                        pixels_per_cell=self.pixels_per_cell,
+#                        cells_per_block=self.cells_per_block,
+#                        block_norm=self.block_norm)
+#
+#         try:  # parallel
+#             return np.array([local_hog(img) for img in X])
+#         except:
+#             return np.array([local_hog(img) for img in X])
 
 
 # create an instance of each transformer
